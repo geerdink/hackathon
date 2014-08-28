b2adab843f731f3f1218d7784da092f6aeec4d04
Bas Geerdink <bas@gdink.com>
2014-08-28 19:10:11 +0200

    fixed callback

--- src/app/js/controllers/inputController.js
+++ src/app/js/controllers/inputController.js
@@ -5,12 +5,13 @@ angular.module('MortgageAdviser')
         $scope.spendingAmount = 0;
         $scope.interestRateMortgage = 4.2;
         $scope.interestRateSavings = 1.6;
+        $scope.adviceResult = "";
 
         $scope.createAdvice = function(){
 
             function onSuccess(response) {
-                alert(JSON.stringify(response));
-                $scope.adviceResult = response;
+                $scope.adviceResult = JSON.stringify(response);
+	            $scope.$digest();
             }
 
             adviceService.createAdvice([$scope.spendingAmount, $scope.availableAmount, $scope.interestRateMortgage, $scope.interestRateSavings], onSuccess);
