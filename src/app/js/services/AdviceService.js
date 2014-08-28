1c45dc16406e5993cee4f125ecb012dc3fcc94fc
bweefting <base@infosupport.com>
2014-08-28 14:31:29 +0200

    Integrated dummy adviceService

--- src/app/js/services/AdviceService.js
+++ src/app/js/services/AdviceService.js
@@ -1,14 +1,45 @@
 'use strict';
 
-/* Services */
+angular.module('MortgageAdviser')
+    .factory('adviceService', ['$resource', function($resource){
+        var url = '/api/advice';
 
-var adviceServices = angular.module('MortgageAdviser');
+//        return $resource(url, {}, {
+//            createAdvice: {method:'POST'}
+//        });
 
-adviceServices.factory('AdviceService', ['$resource',
-    function($resource){
-        var url = '/advice';
+        var mockOutput = {
+            "kind": "prediction#output",
+            "id": "test1234",
+            "selfLink": "https://www.googleapis.com/prediction/v1.6/projects/disco-rope-684/trainedmodels/test1234/predict",
+            "outputLabel": "Sparen",
+            "outputMulti": [
+                {
+                    "label": "Aflossen",
+                    "score": "2.500000"
+                },
+                {
+                    "label": "Sparen",
+                    "score": "6.000000"
+                },
+                {
+                    "label": "Beleggen",
+                    "score": "1.500000"
+                }
+            ]
+        };
 
-        return $resource(url, {}, {
-            createAdvice: {method:'POST'}
-        });
+
+        return {
+            createAdvice: function(data) {
+                return {
+                    then: function (onSuccess, onFailure) {
+                        onSuccess(mockOutput.outputLabel);
+                        onFailure();
+                    }
+                }
+            }
+        }
+
+
     }]);
